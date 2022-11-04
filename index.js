72e4bd88fe2a0937ad296ed0a3ac005e49c347b6
Vittorio <vittfassano@gmail.com>
2022-11-04 16:34:13 -0300

    Init & post sign-up

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const users = [];
+
+app.post("/sign-up", (req, res) => {
+    const {username, avatar} = req.body;
+
+    const newUser = {
+        username,
+        avatar,
+    };
+
+    users.push(newUser);
+    res.status(201).send("OK");
+
+    console.log(users);
+});
 
 
 app.listen(5000, () => {
